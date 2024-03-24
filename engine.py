c5a8da1cc95a68e57f35b6312affc318e18bcfaf
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2024-03-24 11:57:18 +0100

    engine: CodeBuilder class
    This is a helper class used by Template class
    to keep track of the Python code to be generated.
    It is responsible for:
      - adding single line of code
      - managing indentation
      - keeping track of the whole code

--- engine.py
+++ engine.py
@@ -1,4 +1,25 @@
 
+class CodeBuilder:
+
+    INDENT_STEPS = 4
+
+    def __init__(self):
+        self.code = []
+        self.indent_level = 0
+
+    def add_line(self, line):
+        self.code.extend([' '*self.indent_level, line, '\n'])
+
+    def indent(self):
+        self.indent_level += self.INDENT_STEPS
+
+    def dedent(self):
+        self.indent_level -= self.INDENT_STEPS
+
+    def __str__(self):
+        return "".join(self.code)
+
+
 class Template:
     def __init__(self, text, *contexts):
         """
